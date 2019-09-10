6532318ca8471d3da65dcee632eb7165ec6fd3bb
Thor <thor.henriksen.97@gmail.com>
2019-09-10 11:52:58 +0200

    PN fixed

--- ProjektTest/ordination/PN.java
+++ ProjektTest/ordination/PN.java
@@ -1,45 +1,56 @@
 package ordination;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
-public class PN {
+public class PN extends Ordination {
 
-    private double antalEnheder;
+	private double antalEnheder;
+	private ArrayList<LocalDate> datoerGivet = new ArrayList<>();
 
-    /**
-     * Registrerer at der er givet en dosis paa dagen givesDen
-     * Returnerer true hvis givesDen er inden for ordinationens gyldighedsperiode og datoen huskes
-     * Retrurner false ellers og datoen givesDen ignoreres
-     * @param givesDen
-     * @return
-     */
-    public boolean givDosis(LocalDate givesDen) {
-        // TODO
-        return false;   
-    }
+	public PN(LocalDate startDen, LocalDate slutDen, Laegemiddel laegemiddel, double antalEnheder) {
+		super(startDen, slutDen, laegemiddel);
+		this.antalEnheder = antalEnheder;
+	}
 
-    public double doegnDosis() {
-        // TODO
-        return 0.0;
-    }
+	/**
+	 * Registrerer at der er givet en dosis paa dagen givesDen Returnerer true hvis
+	 * givesDen er inden for ordinationens gyldighedsperiode og datoen huskes
+	 * Retrurner false ellers og datoen givesDen ignoreres
+	 * 
+	 * @param givesDen
+	 * @return
+	 */
+	public boolean givDosis(LocalDate givesDen) {
+		if (givesDen.compareTo(super.getStartDen()) >= 0 && givesDen.compareTo(super.getSlutDen()) <= 0) {
+			datoerGivet.add(givesDen);
+			return true;
+		}
+		return false;
+	}
 
+	public double doegnDosis() {
+		return (datoerGivet.size() * antalEnheder) / super.antalDage();
+	}
 
-    public double samletDosis() {
-        // TODO
-        return 0.0;
-    }
+	public double samletDosis() {
+		return datoerGivet.size() * antalEnheder;
+	}
 
-    /**
-     * Returnerer antal gange ordinationen er anvendt
-     * @return
-     */
-    public int getAntalGangeGivet() {
-        // TODO
-        return-1;
-    }
+	/**
+	 * Returnerer antal gange ordinationen er anvendt
+	 * 
+	 * @return
+	 */
+	public int getAntalGangeGivet() {
+		return datoerGivet.size();
+	}
 
-    public double getAntalEnheder() {
-        return antalEnheder;
-    }
+	public double getAntalEnheder() {
+		return antalEnheder;
+	}
 
+	public String getType() {
+		return "PN";
+	}
 }
