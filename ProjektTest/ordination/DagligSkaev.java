8e61ac4650779ae2e5ade65d741d509e858788af
Jacob Schlein <jacobschlein@gmail.com>
2019-09-10 11:15:33 +0200

    Added dagligSkaev

--- ProjektTest/ordination/DagligSkaev.java
+++ ProjektTest/ordination/DagligSkaev.java
@@ -1,12 +1,42 @@
 package ordination;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
 
-public class DagligSkaev {
-    // TODO
+public class DagligSkaev extends Ordination {
+    
+	private ArrayList<Dosis> doser;
 
-
-    public void opretDosis(LocalTime tid, double antal) {
-        // TODO
+	public void opretDosis(LocalTime tid, double antal) {
+        Dosis dosis = new Dosis(tid,antal);
+        doser.add(dosis);
+        
     }
+
+	@Override
+	public double samletDosis() {
+		double sum = 0;
+		for(Dosis d : doser) {
+			sum += d.getAntal();
+		}
+		
+		return sum * super.antalDage();
+	}
+
+	@Override
+	public double doegnDosis() {
+		double sum = 0;
+		for(Dosis d : doser) {
+			sum += d.getAntal();
+		}
+		
+		return sum;
+	}
+
+
+	@Override
+	public String getType() {
+		
+		return "Skaev";
+	}
 }
